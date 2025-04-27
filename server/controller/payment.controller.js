fa9b63023da2d5cc3ddcb05cd06d43a3c868037d
EtimbukUdofia <etimbukudofia9@gmail.com>
2025-04-27 20:13:34 +0100

    verify payment and generate qrcode

--- server/controller/payment.controller.js
+++ server/controller/payment.controller.js
@@ -1,35 +1,30 @@
 import axios from "axios";
+import qrcode from "qrcode";
+import { sendQRCodeEmail } from "../services/mailer.js";
+import { QrCode } from "../model/QrCode.js";
 
 export const initializePayment = async (req, res) => {
   const { email } = req.body;
 
-  // const options = {
-  //   hostname: "api.paystack.co",
-  //   port: 443,
-  //   path: "/transaction/initialize",
-  //   method: "POST",
-  //   headers: {
-  //     Authorization: "Bearer SECRET_KEY",
-  //     "Content-Type": "application/json",
-  //   },
-  // };
   try {
-    const response = await axios.post('https://api.paystack.co/transaction/initialize', {
-      email,
-      amount: 5000,
-      callback_url: `${process.env.BASE_URL}/api/v0/payment-callback`,
-    },
+    const response = await axios.post(
+      "https://api.paystack.co/transaction/initialize",
+      {
+        email,
+        amount: 5000,
+        callback_url: `${process.env.BASE_URL}/api/v0/payment/payment-callback`,
+      },
       {
         headers: {
           Authorization: `Bearer ${process.env.PAYSTACK_SECRET}`,
-          "Content-Type": 'application/json'
+          "Content-Type": "application/json",
         },
-      });
-    
-    const data = response.data;
+      }
+    );
+
     const authorizationURL = response.data.data.authorization_url;
 
-    res.status(200).json(data);
+    res.status(200).json({ authorization_url: authorizationURL });
   } catch (error) {
     console.error(error.response ? error.response.data : error.message);
     res
@@ -41,7 +36,44 @@ export const initializePayment = async (req, res) => {
 export const verifyPaymentAndGenerateQR = async (req, res) => {
   const { reference } = req.query;
 
+  if (!reference) {
+    return res.redirect(`${process.env.FRONTEND_URL}/payment-failed?message=Missing%20Reference`);
+  }
+
+  console.log("passed");
+
   try {
+    const response = await axios.get(
+      `https://api.paystack.co/transaction/verify/${reference}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.PAYSTACK_SECRET}`,
+        },
+      }
+    );
+
+    if (response.data.data.status === 'success') {
+      const email = response.data.data.customer.email;
+      const uniqueCode = `QR-${reference}`;
+
+      //Generate QR Code
+      const qrCodeDataUrl = await qrcode.toDataURL(uniqueCode);
+
+      // save QR to database
+      const result = await QrCode.create({
+        reference,
+        email,
+        qrCodeDataUrl,
+      });
+
+      // Send QR via Email
+      // await sendQRCodeEmail(email, qrCodeDataUrl);
+
+      console.log({ qrcode: qrCodeDataUrl });
+      res.redirect(`${process.env.FRONTEND_URL}result.html?reference=${reference}`);
+    } else {
+      res.status(400).json({ success: false, message: "Payment not successful" });
+    }
   } catch (error) {
     console.error(error.response ? error.response.data : error.message);
     res
