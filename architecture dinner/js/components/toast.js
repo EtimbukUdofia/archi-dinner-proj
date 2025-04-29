b57464c8a9c0ca88df9ef4b71cf25417587fcef6
Jess0707 <124217589+Jess0707@users.noreply.github.com>
2025-04-29 16:44:50 +0100

    make a couple of changes to prevent glitches in animation

--- architecture dinner/js/components/toast.js
+++ architecture dinner/js/components/toast.js
@@ -1,28 +1,21 @@
 function showNotification(message, type = 'success', duration = 8000) {
-   
-    const notification = document.createElement("div");
-    notification.className = `notification ${type}`;
-    notification.textContent = message;
-  
-    document.body.appendChild(notification);
-  
-    
-    requestAnimationFrame(() => {
+  const notification = document.createElement("div");
+  notification.className = `notification ${type}`;
+  notification.textContent = message;
+
+  document.body.appendChild(notification);
+
+  requestAnimationFrame(() => {
       notification.classList.add("show");
-    });
-  
-   
-    setTimeout(() => {
+  });
+
+  setTimeout(() => {
       notification.classList.remove("show");
       setTimeout(() => {
-        notification.remove();
-      }, 500); // wait for transition to finish
-    }, duration);
-  }
-  
-  
-  window.onload = () => {
-    showNotification("Registration successful ! Show this QR code at the event entrance", "success");
-  
-  };
+          notification.remove();
+      }, 500);
+  }, duration);
+}
+
+
   
