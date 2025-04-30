30a2d3ec86d4cb8e69b5553d25a165b2cd30fe1b
EtimbukUdofia <etimbukudofia9@gmail.com>
2025-04-30 04:42:06 +0100

    implemment rate-limiting, compression and error-handling

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import compression from "compression";
 import fs from "fs";
 import path from "path";
 
@@ -16,22 +19,42 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
+app.use(helmet());
 // app.use(cors({origin: process.env.FRONTEND_URL, credentials: true}));
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(morgan('common', {
   skip: function (req, res) { return res.statusCode < 400 }
 }));
-
 // remember to handle exceptions here
 app.use(morgan('common', {
   stream: fs.createWriteStream(path.join(__dirname, 'log', 'access.log'), { flags: 'a' })
 }));
+app.use(compression());
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000, max: 100
+}));
 
+app.use("/api/vo/home", (req, res) => {
+  res.send("Working");
+})
 app.use("/api/v0/payment", paymentRoutes);
 app.use("/api/v0", qrRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "🔍 Endpoint not found",
+  });
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
 
 app.listen(PORT, () => {
   connectDB();
